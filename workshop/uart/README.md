a7e8e6ba4ef251b4cf87fc1230c15c83bcb7506e
ecrecords <elvischino754@gmail.com>
2022-04-10 09:19:35 -0700

    ecrecords: updated uart demo with correct sleep timing and updated readme

--- workshop/uart/README.md
+++ workshop/uart/README.md
@@ -1,4 +1,20 @@
-# UART Demo
+# Universal Asynchronous Receiver Transmitter (UART)
+
+## Introduction
+Universal asynchronous receiver transmitter (UART) is a serial commination protocol that is used to communicate between any two devices.
+It is an asynchronous protocol, therefore is has no need for a clock. It utilizes only two connections to function. UART delivers a message using a 11/12 bit packet. The frame can be seen bellow.
+
+<p align="center">
+<img src="../../img/uart_dark.png" width="800">
+<br> <b> UART Frame </b>
+</p>
+
+When design a application that utilizes UART the following parameters must be considered:
+- `START` bit
+- `DATA` byte
+- `PARITY` bit (optional)
+- `STOP` bit
+
 
 ## [Python Code](uart_demo.py)
 ## Wiring Diagram
--- workshop/uart/uart_demo.py
+++ workshop/uart/uart_demo.py
@@ -8,39 +8,53 @@
 from machine import UART, Pin
 import utime
 
-baud = 9600
+# Rate at which UART communicates (bits per second)
+BAUD = 9600
+# Num of START bits
+START_BITS = 1
+# Num of STOP bits
+STOP_BITS = 1
+# Num of PARITY bits
+PARITY_BITS = 0
+# PARITY bit type
+PARITY_TYPE = None
+# BAUD rate converted to bytes per second
+BYTES_PER_SEC = 1/(BAUD/8)
 
 # Declares a 32 byte buffer
-read_bytes = bytearray(32)
+write_bytes = bytearray()
+read_bytes = ""
 
 # Declares and initializes UART0 using GP0 (TX) and GP1(RX)
 tx_flag = Pin(2, mode=Pin.IN, pull=Pin.PULL_UP)
 rx_flag = Pin(3, mode=Pin.IN, pull=Pin.PULL_DOWN)
 
-uart0 = UART(id=0, baudrate=baud,tx=Pin(0), rx=Pin(1))
+uart0 = UART(id=0, baudrate=BAUD, stop=STOP_BITS, tx=Pin(0), rx=Pin(1))
 
 while True:
 
     if tx_flag:
         # Ask for string via REPL
-        outp_str = input("UART Message: ")
-
+        output_str = input("UART Message: ")
+        write_bytes = bytearray(len(output_str))
         # Encodes string (ASCII)
-        write_bytes = outp_str.encode()
+        write_bytes = output_str.encode()
 
         # Writes message via GP0
         uart0.write(write_bytes)
 
+        # Calculates amount of time CPU must wait for UART to fully send message
+        sleep_time = BYTES_PER_SEC * (START_BITS + len(write_bytes) + PARITY_BITS + STOP_BITS)
         # CPU sleeps for msg to be fully sent
-        utime.sleep_ms( round( (len(outp_str)/9600) * 1E3) )
+        utime.sleep_ms(round(sleep_time * 1E3))
     
 
     if rx_flag:
         if uart0.any() > 0:
             # Dump n bytes into buffer
-            uart0.readinto(read_bytes, len(read_bytes))
+            uart0.readline()
             # Print the received bytes via REPL
-            print(read_bytes.decode())
+            print(read_bytes)
 
     # CPU sleeps for 1 ms
     utime.sleep_ms(1)
