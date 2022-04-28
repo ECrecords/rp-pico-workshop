f32b4910ee7058ab29aa0f1ee77dbd462feec10e
ECrecords <elvis.chinoislas.271@my.csun.edu>
2022-04-28 12:07:10 -0700

    ecrecords: added ssd1306 driver and updated i2c demo

--- workshop/i2c/i2c_demo.py
+++ workshop/i2c/i2c_demo.py
@@ -6,15 +6,26 @@
 #   RP-RP2 by controling a temperature and humidity sensor (AHT10)
 
 from machine import I2C, Pin
-from AHT10 import AHT10
+from aht10 import AHT10
+from ssd1306 import SSD1306_I2C
 
 if __name__ == "__main__":
     # create an I2C object at GP3 and GP2
-    i2c = I2C(id=1, scl=Pin(3), sda=Pin(2), freq=400000)  # type: ignore
+    i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=400000)  # type: ignore
+
+    # scan the i2c bus and store all device addresses into list
+    i2c_dev = i2c.scan()
+
+    # print all address found in the i2c bus onto REPL
+    print("Device on I2C bus:", end=" ")
+    for dev in i2c_dev:
+        print(hex(dev), end=' ')
+    print("\n")
 
     # create an AHT10 object
     sense = AHT10(i2c)
 
+    # display current temperature and rh onto REPL
     print("Current Temperature and Relative Humidity")
     while True:
         print(
